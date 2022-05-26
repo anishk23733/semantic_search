e0cab8a5abd609fd6f47f2db798f47c04d244223
Anish Kachinthaya <anishk@freddie.Millennium.berkeley.edu>
2022-05-26 16:19:57 -0700

    added prompts for text search and file selection

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import numpy as np
 import nltk.data
 from scipy import spatial
 
+filename = input(">> Enter file to search >: ")
+
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-s = open('data/big.txt', 'r') # specify the file we are searching through
+s = open(filename, 'r') # specify the file we are searching through
 
 search_text = s.read()
 sentences = [s.replace('\n', ' ').replace('\t', ' ').strip() for s in tokenizer.tokenize(search_text)]
 
+print(">> Building search cache...")
 embeddings = {sentence: generate_text_embedding(sentence).cpu().detach().numpy() for sentence in sentences if len(sentence) < 70}
+print(">> Completed.\n")
 
 def search(query):
     query_embedding = generate_text_embedding(query).cpu().detach().numpy()
@@ -17,6 +21,13 @@ def search(query):
     # get_dist = lambda sentence: np.linalg.norm(query_embedding - embeddings[sentence])
 
     semantic_similarity = list(sorted(embeddings, key=get_dist))[:20]
-    print(f"Search query: {query}")
-    print("\n".join(semantic_similarity))
-    print("\n\n\n")
+    print(f">> Search query: {query}")
+    print('> ' + "\n> ".join(semantic_similarity))
+    print("\n")
+
+while True:
+    query = input(">: ")
+    if query == 'q':
+        break
+    print()
+    search(query)
